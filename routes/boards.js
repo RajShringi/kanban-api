b60cd220ba952bac432f200b6bf02f12dcb37802
raj shringi <raj24shringi@gmail.com>
2024-03-04 23:21:30 +0530

    add routes for creating board and get user boards

--- routes/boards.js
+++ routes/boards.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const Board = require("../models/Board");
 const router = express.Router();
+const auth = require("../auth/auth");
+const User = require("../models/User");
+
+// check user athentication
+router.use(auth.verifyToken);
 
 // create board
 router.post("/", async (req, res, next) => {
   try {
     const board = await Board.create(req.body.board);
+    //add this newly created board in user boards array
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      { $push: { boards: board.id } },
+      { new: true }
+    );
     res.status(200).json({ board });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// get user boards
+router.get("/", async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+    const userWithPopulateBoards = await user.populate("boards");
+    res.status(200).json({ userWithPopulateBoards });
   } catch (error) {
     next(error);
   }
