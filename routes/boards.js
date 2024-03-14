25286a2b2ba566ff56010d9eb10ba45e0085a872
raj shringi <raj24shringi@gmail.com>
2024-03-14 23:10:22 +0530

    create routes for board according to UI design so board and columns can modify

--- routes/boards.js
+++ routes/boards.js
@@ -1,34 +1,119 @@
 const express = require("express");
 const Board = require("../models/Board");
+const Column = require("../models/Column");
 const router = express.Router();
 const auth = require("../auth/auth");
-const User = require("../models/User");
 
 // check user athentication
 router.use(auth.verifyToken);
 
-// create board
+// Create Board
 router.post("/", async (req, res, next) => {
+  const boardData = {
+    name: req.body.board.name,
+    user: req.user.id,
+    columns: [],
+  };
+
   try {
-    const board = await Board.create(req.body.board);
-    //add this newly created board in user boards array
-    const user = await User.findByIdAndUpdate(
-      req.user.id,
-      { $push: { boards: board.id } },
-      { new: true }
-    );
+    const board = await Board.create(boardData);
+    async function createColumns(board) {
+      for (const column of req.body.board.columns) {
+        const columnData = {
+          name: column,
+          board: board.id,
+          tasks: [],
+        };
+        const createdColumn = await Column.create(columnData);
+        await Board.findByIdAndUpdate(
+          board.id,
+          { $push: { columns: createdColumn.id } },
+          { new: true }
+        );
+      }
+      return await Board.findById(board.id);
+    }
+
+    // Call the function with the board object
+    const updatedBoard = await createColumns(board);
+    res.status(200).json({ updatedBoard });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:boardId", async (req, res, next) => {
+  try {
+    const boardId = req.params.boardId;
+    const board = await Board.findById(boardId);
+    if (!board) {
+      return res.status(400).json("board Not Found");
+    }
+    board.name = req.body.board.name;
+    const updateColumns = async () => {
+      for (const column of req.body.board.columns) {
+        try {
+          if (column.action === "add") {
+            const newColumn = await Column.create({
+              name: column.name,
+              board: boardId,
+              tasks: [],
+            });
+            board.columns.push(newColumn.id);
+          }
+
+          if (column.action === "delete") {
+            const deletedColumn = await Column.findByIdAndDelete(column.id);
+            const index = board.columns.indexOf(deletedColumn.id);
+            board.columns.splice(index, 1);
+          }
+
+          if (column.action === "update") {
+            const updatedColumn = await Column.findByIdAndUpdate(column.id, {
+              name: column.name,
+            });
+          }
+        } catch (error) {
+          return res
+            .status(400)
+            .json({ errMsg: `Error updating column ${column.id}` });
+        }
+      }
+      await board.save();
+      return board;
+    };
+
+    const updatedBoard = await updateColumns();
+    res.status(200).json({ updatedBoard });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Get Board
+router.get("/:boardId", async (req, res, next) => {
+  try {
+    const boardId = req.params.boardId;
+    const board = await Board.findById(boardId);
+    if (!board) {
+      return res.status(400).json({ msg: "board Not Found" });
+    }
     res.status(200).json({ board });
   } catch (error) {
     next(error);
   }
 });
 
-// get user boards
-router.get("/", async (req, res, next) => {
+//Delete Board
+router.delete("/:boardId", async (req, res, next) => {
   try {
-    const user = await User.findById(req.user.id);
-    const userWithPopulateBoards = await user.populate("boards");
-    res.status(200).json({ userWithPopulateBoards });
+    const boardId = req.params.boardId;
+    const board = await Board.findByIdAndDelete(boardId);
+    if (!board) {
+      return res.status(400).json({ msg: "board Not Found" });
+    }
+    await Column.deleteMany({ board: boardId });
+    res.status(200).json({ msg: "board deleted successfully" });
   } catch (error) {
     next(error);
   }
