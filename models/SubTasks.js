49e3d49b5fa57c03cb028e5eec888e5a048a94d7
raj shringi <raj24shringi@gmail.com>
2024-04-10 23:05:26 +0530

    add SubTask schema and Task schema

---
+++ models/SubTasks.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const subTasksSchema = new Schema(
+  {
+    name: { type: String, required: true, uniqure: true },
+    isDone: { type: Boolean, default: false, require: true },
+    task: { type: Schema.Types.ObjectId, ref: "Task", required: true },
+  },
+  { timestamps: true }
+);
+
+const SubTask = mongoose.model("Subtask", subTasksSchema);
+
+module.exports = SubTask;
---
+++ models/Task.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    description: { type: String },
+    subTasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+    column: { type: Schema.Types.ObjectId, ref: "Column", required: true },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
