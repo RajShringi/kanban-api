e2b5b3d865a7ae5d2f28f4fcda59d2361861ee46
raj shringi <raj24shringi@gmail.com>
2024-04-10 22:54:44 +0530

    add unique key in column name

--- models/Column.js
+++ models/Column.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const columnSchema = new Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
     tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
     board: { type: Schema.Types.ObjectId, ref: "Board", required: true },
   },
