1e71ca6947c56fc3b9a7c6c54cbb2861861a1cab
raj shringi <raj24shringi@gmail.com>
2024-05-11 14:11:14 +0530

    remove unique validation in name property from columnSchema

--- models/Column.js
+++ models/Column.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const columnSchema = new Schema(
   {
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
     tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
     board: { type: Schema.Types.ObjectId, ref: "Board", required: true },
   },
