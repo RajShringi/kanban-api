2a277b9cddea4c69f2bca33a3ed6ae79a4d40fe8
raj shringi <raj24shringi@gmail.com>
2024-05-11 14:16:51 +0530

    update deleteBoard and updateBoard function according to boardInfo change and get columns also when getting all board info

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -1,5 +1,7 @@
 const Board = require("../models/Board");
 const Column = require("../models/Column");
+const SubTask = require("../models/SubTasks");
+const Task = require("../models/Task");
 
 async function createBoard(req, res, next) {
   const boardData = {
@@ -42,34 +44,40 @@ async function updateBoard(req, res, next) {
     if (!board) {
       return res.status(400).json({ error: "board Not Found" });
     }
+    console.log(req.body.board.columns, "columns");
     board.name = req.body.board.name;
     const updateColumns = async () => {
       for (const column of req.body.board.columns) {
-        try {
-          if (column.action === "add") {
-            const newColumn = await Column.create({
-              name: column.name,
-              board: boardId,
-              tasks: [],
-            });
-            board.columns.push(newColumn.id);
-          }
+        if (column.action === "add") {
+          const newColumn = await Column.create({
+            name: column.name,
+            board: boardId,
+            tasks: [],
+          });
+          board.columns.push(newColumn._id);
+        }
 
-          if (column.action === "delete") {
-            const deletedColumn = await Column.findByIdAndDelete(column.id);
-            const index = board.columns.indexOf(deletedColumn.id);
-            board.columns.splice(index, 1);
+        if (column.action === "delete") {
+          const deletedColumn = await Column.findByIdAndDelete(column._id);
+          if (!deletedColumn) {
+            return res
+              .status(400)
+              .json({ error: `Column ${column._id} not found` });
           }
+          // Delete associated tasks of the deleted column
+          await Task.deleteMany({ column: deletedColumn._id });
 
-          if (column.action === "update") {
-            const updatedColumn = await Column.findByIdAndUpdate(column.id, {
-              name: column.name,
-            });
-          }
-        } catch (error) {
-          return res
-            .status(400)
-            .json({ error: `Error updating column ${column.id}` });
+          // Delete associated subtasks of the deleted tasks
+          await SubTask.deleteMany({ task: { $in: deletedColumn.tasks } });
+
+          const index = board.columns.indexOf(deletedColumn._id);
+          board.columns.splice(index, 1);
+        }
+
+        if (column.action === "update") {
+          const updatedColumn = await Column.findByIdAndUpdate(column._id, {
+            name: column.name,
+          });
         }
       }
       await board.save();
@@ -99,12 +107,34 @@ async function getBoardInfo(req, res, next) {
 async function deleteBoard(req, res, next) {
   try {
     const boardId = req.params.boardId;
-    const board = await Board.findByIdAndDelete(boardId);
-    if (!board) {
-      return res.status(400).json({ error: "board Not Found" });
-    }
+    // Find all columns of the board
+    const columns = await Column.find({ board: boardId });
+
+    // Extract all task IDs from the columns
+    const taskIds = columns.reduce(
+      (acc, column) => acc.concat(column.tasks),
+      []
+    );
+
+    // Delete all subtasks associated with the tasks of the board
+    await SubTask.deleteMany({ task: { $in: taskIds } });
+
+    // Delete all tasks associated with the columns of the board
+    await Task.deleteMany({
+      column: { $in: columns.map((column) => column._id) },
+    });
+
+    // Delete all columns of the board
     await Column.deleteMany({ board: boardId });
-    res.status(200).json({ msg: "board deleted successfully" });
+
+    // Finally, delete the board itself
+    const deletedBoard = await Board.findByIdAndDelete(boardId);
+
+    // Return success message or any other necessary data
+    return {
+      message:
+        "Board and associated columns, tasks, and subtasks successfully deleted",
+    };
   } catch (error) {
     next(error);
   }
@@ -112,7 +142,9 @@ async function deleteBoard(req, res, next) {
 
 async function getAllBoards(req, res, next) {
   try {
-    const boards = await Board.find({ user: req.user.id }).select("id, name");
+    const boards = await Board.find({ user: req.user.id }).select(
+      "id name columns"
+    );
     res.status(200).json({ boards });
   } catch (error) {
     next(error);
