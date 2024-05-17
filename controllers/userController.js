9ca752daf6d00e11fb7265e46987911f71dc65d7
raj shringi <raj24shringi@gmail.com>
2024-05-18 00:28:55 +0530

    add required field check and check if relevent info is passed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,23 @@
 const User = require("../models/User");
 
 async function userRegister(req, res, next) {
+  const { username, email, password, name } = req.body.user;
   try {
+    if (!email || !password || !username || !name) {
+      return res.status(400).json({ errMsg: "All Fields are Required" });
+    }
+    const findUserByEmail = await User.findOne({ email });
+    if (findUserByEmail) {
+      return res
+        .status(400)
+        .json({ errMsg: "User with this email already exist" });
+    }
+    const findUserByUsername = await User.findOne({ username });
+    if (findUserByUsername) {
+      return res
+        .status(400)
+        .json({ errMsg: "User with this username already exist" });
+    }
     const user = await User.create(req.body.user);
     const token = await user.generateToken();
     res.status(200).json(user.userInfo(token));
