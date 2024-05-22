5bdeaf1e13ad31b81309e587be53b1153230af30
raj shringi <raj24shringi@gmail.com>
2024-05-22 17:53:08 +0530

    add route for check server is running or not

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const { userRegister, userLogin } = require("../controllers/userController");
 const router = express.Router();
 
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  res.status(200).json({ msg: "Server is Running" });
+});
+
 // user register route
 router.post("/signup", userRegister);
 
